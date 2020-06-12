caabc8a0231d924dba6fae6f712fae01dc82f9da
sampu <sampu@LAPTOP-CP78HEF5>
2020-06-12 09:09:06 +0530

    Loops Ex1

---
+++ Milestone1/src/com/wipro/loops/Ex1.java
@@ -0,0 +1,17 @@
+package com.wipro.loops;
+
+public class Ex1 {
+	public static void main(String[] args) {
+	int x=Integer.parseInt(args[0]);
+	if(x>0)
+	{
+		System.out.println("Positive");
+	}
+	else if(x<0)
+	{
+		System.out.println("Negative");
+	}
+	else
+		System.out.println("Zero");
+}
+}
